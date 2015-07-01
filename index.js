3cade5e2186ead72967a4ed43f101ab2ac441276
loicgasser <loicgasser4@gmail.com>
2015-07-01 16:40:29 +0200

    Ranges can be different according to the zoom level

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 var http = require('http');
 var httpProxy = require('http-proxy');
 
-var zoomRange = [17, 17];
-var xRange = [136772, 136814];
-var yRange = [99527, 99547];
+var zoomRange = [15, 17];
+
+var config = {
+  15: {
+    xRange: [34190, 34191],
+    yRange: [24881, 24882]
+  },
+  16: {
+    xRange: [68381, 68402],
+    yRange: [49763, 49783]
+  },
+  17: {
+    xRange: [136772, 136804],
+    yRange: [99527, 99566]
+  }
+};
 
 var inRange = function(val, range) {
   if (val >= range[0] && val <= range[1]) {
@@ -29,12 +42,15 @@ http.createServer(function(req, client_res) {
     var x = parseInt(matches[2]);
     var y = parseInt(matches[3]);
     if (!isNaN(zoom) && !isNaN(x) && !isNaN(y)) {
-      if (inRange(zoom, zoomRange) &&
-          inRange(x, xRange) &&
-          inRange(y, yRange)) {
-        options.hostname = 'tms3d.geo.admin.ch.s3.amazonaws.com'
-        options.path = req.url.split('/tiles')[1]
-        console.log('Going with our tiles now!' + options.hostname + options.path);
+      if (inRange(zoom, zoomRange)) {
+        var xRange = config[zoom].xRange;
+        var yRange = config[zoom].yRange;
+        if (inRange(x, xRange) &&
+            inRange(y, yRange)) {
+          options.hostname = 'tms3d.geo.admin.ch.s3.amazonaws.com'
+          options.path = req.url.split('/tiles')[1]
+          console.log('Going with our tiles now!' + options.hostname + options.path);
+        }
       }
     }
   }
@@ -45,4 +61,3 @@ http.createServer(function(req, client_res) {
   });
 
 }).listen(9014);
-
